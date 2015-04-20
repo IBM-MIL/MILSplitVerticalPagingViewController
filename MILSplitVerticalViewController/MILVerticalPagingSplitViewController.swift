d9c271ba0a91b1657165ae2e96eb40f875c11139
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-20 14:24:10 -0400

    places IBAction method below init & helpers, above private instance methods

--- MILSplitVerticalViewController/MILVerticalPagingSplitViewController.swift
+++ MILSplitVerticalViewController/MILVerticalPagingSplitViewController.swift
@@ -57,228 +57,6 @@ class VerticalPagingSplitViewController: UIViewController {
     
     
     // Instance Methods
-    // Action Methods
-    // This function does a lot of stuff, and gets pretty crazy. But the gist of it is this:
-    // - Determine which side of the screen the pan is on
-    // - Determine the direction and whether or not we want to count it as a swipe
-    // - Determine if a view controller exists in the direction we are moving to
-    // - Start the transistion to that view controller
-    // - When the gesture ends, determine which view controller to completely transistion to
-    @IBAction func PanGestureRecognized(sender: UIPanGestureRecognizer) {
-        
-        let halfwayMark = self.view.frame.size.height / 2
-        var resetTranslation = true
-        
-        // Get the direction of this pan gesture
-        let direction = (sender.velocityInView(self.view).y > 0) ? Direction.Up : Direction.Down
-        
-        let swipe = determineSwipeDirection(sender)
-        
-        // On start, create the view controllers above and below the current view controller
-        if sender.state == UIGestureRecognizerState.Began {
-            
-            var startPoint = sender.locationOfTouch(0, inView: self.view)
-            
-            // Determine the side of the swipe
-            if startPoint.x < (self.view.frame.size.width / 2) {
-                
-                side = .Left
-                containerForGesture = leftContainerView
-                indexForGesture = currentLeftVCIndex
-                arrayForGesture = leftViewControllers
-                currentVCForGesture = currentLeftVC
-                
-            }
-            
-            else {
-                
-                side = .Right
-                containerForGesture = rightContainerView
-                indexForGesture = currentRightVCIndex
-                arrayForGesture = rightViewControllers
-                currentVCForGesture = currentRightVC
-                
-            }
-            
-            // Frames for view controllers
-            upDirectionEndFrame = CGRect(
-                x: 0,
-                y: -containerForGesture!.frame.size.height,
-                width: containerForGesture!.frame.size.width,
-                height: containerForGesture!.frame.size.height)
-            
-            normalEndFrame = CGRect(
-                x: 0,
-                y: 0,
-                width: containerForGesture!.frame.size.width,
-                height: containerForGesture!.frame.size.height)
-            
-            downDirectionEndFrame = CGRect(
-                x: 0,
-                y: containerForGesture!.frame.size.height,
-                width: containerForGesture!.frame.size.width,
-                height: containerForGesture!.frame.size.height)
-            
-            // Determine if we can create view controller above/below the moving view controller
-            canGoToAboveVC = true
-            canGoToBelowVC = true
-            
-            if indexForGesture == 0 {
-                canGoToAboveVC = false
-            }
-            
-            if indexForGesture == arrayForGesture!.count - 1 {
-                canGoToBelowVC = false
-            }
-            
-            // Create view controllers if possible and set above and below the current view controller
-            if canGoToBelowVC {
-                
-                belowVC = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier(arrayForGesture![indexForGesture! + 1]) as? UIViewController
-                self.addChildViewController(belowVC!)
-                containerForGesture!.addSubview(belowVC!.view)
-                belowVC!.view.frame = downDirectionEndFrame
-                
-            }
-            
-            else {
-                belowVC = nil
-            }
-            
-            if canGoToAboveVC {
-                
-                aboveVC = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier(arrayForGesture![indexForGesture! - 1]) as? UIViewController
-                self.addChildViewController(aboveVC!)
-                containerForGesture!.addSubview(aboveVC!.view)
-                aboveVC!.view.frame = upDirectionEndFrame
-                
-            }
-            
-            else {
-                aboveVC = nil
-            }
-            
-        }
-        
-        else if sender.state == UIGestureRecognizerState.Ended {
-            
-            // When the gesture ends, determine which view controller that needs to become the current view controller and finish the transistion
-            var resetToCurrentVC = false
-            
-            if swipe {
-                
-                if direction == .Down && canGoToBelowVC {
-                    moveFromViewController(currentVCForGesture!, toViewController: belowVC!, direction: direction, side: side)
-                    aboveVC?.willMoveToParentViewController(nil)
-                    aboveVC?.removeFromParentViewController()
-                }
-                
-                else if direction == .Up && canGoToAboveVC {
-                    moveFromViewController(currentVCForGesture!, toViewController: aboveVC!, direction: direction, side: side)
-                    belowVC?.willMoveToParentViewController(nil)
-                    belowVC?.removeFromParentViewController()
-                }
-                
-                else {
-                    resetToCurrentVC = true
-                }
-                
-                swipe = false
-                
-            }
-                
-            else {
-                
-                if currentVCForGesture!.view.frame.origin.y <= -halfwayMark {
-                    moveFromViewController(currentVCForGesture!, toViewController: belowVC!, direction: .Down, side: side)
-                    aboveVC?.willMoveToParentViewController(nil)
-                    aboveVC?.removeFromParentViewController()
-                }
-                
-                else if currentVCForGesture!.view.frame.origin.y > halfwayMark {
-                    moveFromViewController(currentVCForGesture!, toViewController: aboveVC!, direction: .Up, side: side)
-                    belowVC?.willMoveToParentViewController(nil)
-                    belowVC?.removeFromParentViewController()
-                }
-                
-                else {
-                    resetToCurrentVC = true
-                }
-                
-            }
-            
-            if resetToCurrentVC {
-                
-                if currentVCForGesture!.view.frame.origin.y > 0 {
-                    
-                    moveFromViewController(aboveVC!, toViewController: currentVCForGesture!, direction: .Down, side: side)
-                    belowVC?.willMoveToParentViewController(nil)
-                    belowVC?.removeFromParentViewController()
-                    
-                }
-                
-                else if currentVCForGesture!.view.frame.origin.y < 0 {
-                    
-                    moveFromViewController(belowVC!, toViewController: currentVCForGesture!, direction: .Up, side: side)
-                    aboveVC?.willMoveToParentViewController(nil)
-                    aboveVC?.removeFromParentViewController()
-                    
-                }
-                
-            }
-            
-        }
-        
-        else {
-            
-            // While the gesture is occuring, determine if we should move the view controllers along with it
-            var yOffset = sender.translationInView(self.view).y
-            var applyOffset = false
-            
-            if yOffset > 0 {
-                
-                if canGoToAboveVC || (currentVCForGesture!.view.frame.origin.y < 0) {
-                    
-                    applyOffset = true
-                    
-                    if (currentVCForGesture!.view.frame.origin.y < 0) && (currentVCForGesture!.view.frame.origin.y + yOffset > 0) {
-                        yOffset = -(currentVCForGesture!.view.frame.origin.y)
-                    }
-                    
-                }
-                
-            }
-            
-            else if yOffset < 0 {
-                
-                if canGoToBelowVC || (currentVCForGesture!.view.frame.origin.y > 0) {
-                    
-                    applyOffset = true
-                    
-                    if (currentVCForGesture!.view.frame.origin.y > 0) && (currentVCForGesture!.view.frame.origin.y + yOffset < 0) {
-                        yOffset = -currentVCForGesture!.view.frame.origin.y
-                    }
-                    
-                }
-                
-            }
-            
-            if applyOffset {
-                
-                currentVCForGesture!.view.frame.offset(dx: 0, dy: yOffset)
-                aboveVC?.view.frame.offset(dx: 0, dy: yOffset)
-                belowVC?.view.frame.offset(dx: 0, dy: yOffset)
-                
-            }
-            
-        }
-        
-        if resetTranslation {
-            sender.setTranslation(CGPointZero, inView: self.view)
-        }
-        
-    }
-    
     private func determineSwipeDirection(sender: UIPanGestureRecognizer) -> Bool {
         
         // Detect if this gesture is moving very quickly (might be a swipe)
@@ -383,6 +161,227 @@ class VerticalPagingSplitViewController: UIViewController {
     
     
     // All other methods
+    // Action Methods
+    // This function does a lot of stuff, and gets pretty crazy. But the gist of it is this:
+    // - Determine which side of the screen the pan is on
+    // - Determine the direction and whether or not we want to count it as a swipe
+    // - Determine if a view controller exists in the direction we are moving to
+    // - Start the transistion to that view controller
+    // - When the gesture ends, determine which view controller to completely transistion to
+    @IBAction func PanGestureRecognized(sender: UIPanGestureRecognizer) {
+        
+        let halfwayMark = self.view.frame.size.height / 2
+        var resetTranslation = true
+        
+        // Get the direction of this pan gesture
+        let direction = (sender.velocityInView(self.view).y > 0) ? Direction.Up : Direction.Down
+        
+        let swipe = determineSwipeDirection(sender)
+        
+        // On start, create the view controllers above and below the current view controller
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            var startPoint = sender.locationOfTouch(0, inView: self.view)
+            
+            // Determine the side of the swipe
+            if startPoint.x < (self.view.frame.size.width / 2) {
+                
+                side = .Left
+                containerForGesture = leftContainerView
+                indexForGesture = currentLeftVCIndex
+                arrayForGesture = leftViewControllers
+                currentVCForGesture = currentLeftVC
+                
+            }
+                
+            else {
+                
+                side = .Right
+                containerForGesture = rightContainerView
+                indexForGesture = currentRightVCIndex
+                arrayForGesture = rightViewControllers
+                currentVCForGesture = currentRightVC
+                
+            }
+            
+            // Frames for view controllers
+            upDirectionEndFrame = CGRect(
+                x: 0,
+                y: -containerForGesture!.frame.size.height,
+                width: containerForGesture!.frame.size.width,
+                height: containerForGesture!.frame.size.height)
+            
+            normalEndFrame = CGRect(
+                x: 0,
+                y: 0,
+                width: containerForGesture!.frame.size.width,
+                height: containerForGesture!.frame.size.height)
+            
+            downDirectionEndFrame = CGRect(
+                x: 0,
+                y: containerForGesture!.frame.size.height,
+                width: containerForGesture!.frame.size.width,
+                height: containerForGesture!.frame.size.height)
+            
+            // Determine if we can create view controller above/below the moving view controller
+            canGoToAboveVC = true
+            canGoToBelowVC = true
+            
+            if indexForGesture == 0 {
+                canGoToAboveVC = false
+            }
+            
+            if indexForGesture == arrayForGesture!.count - 1 {
+                canGoToBelowVC = false
+            }
+            
+            // Create view controllers if possible and set above and below the current view controller
+            if canGoToBelowVC {
+                
+                belowVC = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier(arrayForGesture![indexForGesture! + 1]) as? UIViewController
+                self.addChildViewController(belowVC!)
+                containerForGesture!.addSubview(belowVC!.view)
+                belowVC!.view.frame = downDirectionEndFrame
+                
+            }
+                
+            else {
+                belowVC = nil
+            }
+            
+            if canGoToAboveVC {
+                
+                aboveVC = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier(arrayForGesture![indexForGesture! - 1]) as? UIViewController
+                self.addChildViewController(aboveVC!)
+                containerForGesture!.addSubview(aboveVC!.view)
+                aboveVC!.view.frame = upDirectionEndFrame
+                
+            }
+                
+            else {
+                aboveVC = nil
+            }
+            
+        }
+            
+        else if sender.state == UIGestureRecognizerState.Ended {
+            
+            // When the gesture ends, determine which view controller that needs to become the current view controller and finish the transistion
+            var resetToCurrentVC = false
+            
+            if swipe {
+                
+                if direction == .Down && canGoToBelowVC {
+                    moveFromViewController(currentVCForGesture!, toViewController: belowVC!, direction: direction, side: side)
+                    aboveVC?.willMoveToParentViewController(nil)
+                    aboveVC?.removeFromParentViewController()
+                }
+                    
+                else if direction == .Up && canGoToAboveVC {
+                    moveFromViewController(currentVCForGesture!, toViewController: aboveVC!, direction: direction, side: side)
+                    belowVC?.willMoveToParentViewController(nil)
+                    belowVC?.removeFromParentViewController()
+                }
+                    
+                else {
+                    resetToCurrentVC = true
+                }
+                
+                swipe = false
+                
+            }
+                
+            else {
+                
+                if currentVCForGesture!.view.frame.origin.y <= -halfwayMark {
+                    moveFromViewController(currentVCForGesture!, toViewController: belowVC!, direction: .Down, side: side)
+                    aboveVC?.willMoveToParentViewController(nil)
+                    aboveVC?.removeFromParentViewController()
+                }
+                    
+                else if currentVCForGesture!.view.frame.origin.y > halfwayMark {
+                    moveFromViewController(currentVCForGesture!, toViewController: aboveVC!, direction: .Up, side: side)
+                    belowVC?.willMoveToParentViewController(nil)
+                    belowVC?.removeFromParentViewController()
+                }
+                    
+                else {
+                    resetToCurrentVC = true
+                }
+                
+            }
+            
+            if resetToCurrentVC {
+                
+                if currentVCForGesture!.view.frame.origin.y > 0 {
+                    
+                    moveFromViewController(aboveVC!, toViewController: currentVCForGesture!, direction: .Down, side: side)
+                    belowVC?.willMoveToParentViewController(nil)
+                    belowVC?.removeFromParentViewController()
+                    
+                }
+                    
+                else if currentVCForGesture!.view.frame.origin.y < 0 {
+                    
+                    moveFromViewController(belowVC!, toViewController: currentVCForGesture!, direction: .Up, side: side)
+                    aboveVC?.willMoveToParentViewController(nil)
+                    aboveVC?.removeFromParentViewController()
+                    
+                }
+                
+            }
+            
+        }
+            
+        else {
+            
+            // While the gesture is occuring, determine if we should move the view controllers along with it
+            var yOffset = sender.translationInView(self.view).y
+            var applyOffset = false
+            
+            if yOffset > 0 {
+                
+                if canGoToAboveVC || (currentVCForGesture!.view.frame.origin.y < 0) {
+                    
+                    applyOffset = true
+                    
+                    if (currentVCForGesture!.view.frame.origin.y < 0) && (currentVCForGesture!.view.frame.origin.y + yOffset > 0) {
+                        yOffset = -(currentVCForGesture!.view.frame.origin.y)
+                    }
+                    
+                }
+                
+            }
+                
+            else if yOffset < 0 {
+                
+                if canGoToBelowVC || (currentVCForGesture!.view.frame.origin.y > 0) {
+                    
+                    applyOffset = true
+                    
+                    if (currentVCForGesture!.view.frame.origin.y > 0) && (currentVCForGesture!.view.frame.origin.y + yOffset < 0) {
+                        yOffset = -currentVCForGesture!.view.frame.origin.y
+                    }
+                    
+                }
+                
+            }
+            
+            if applyOffset {
+                
+                currentVCForGesture!.view.frame.offset(dx: 0, dy: yOffset)
+                aboveVC?.view.frame.offset(dx: 0, dy: yOffset)
+                belowVC?.view.frame.offset(dx: 0, dy: yOffset)
+                
+            }
+            
+        }
+        
+        if resetTranslation {
+            sender.setTranslation(CGPointZero, inView: self.view)
+        }
+        
+    }
     // Helper for PanGestureRecognized method
     // Called when a gesture ends to transistion from one view controller to another.
     private func moveFromViewController(vc: UIViewController, toViewController: UIViewController, direction: Direction, side: Side) {
