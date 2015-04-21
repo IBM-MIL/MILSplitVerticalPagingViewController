fb72c408e6e04615b7ef992b58c2df6258d25604
Ruslan Ardashev <ardashev120101@gmail.com>
2015-04-20 20:17:51 -0400

    adds a tester project

---
+++ MILVerticalPagingSplitViewControllerTester/MILVerticalPagingSplitViewControllerTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MILVerticalPagingSplitViewControllerTester
+//
+//  Created by Ruslan Ardashev on 4/20/15.
+//  Copyright (c) 2015 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
